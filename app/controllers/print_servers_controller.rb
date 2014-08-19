e5e74be85fbd17a73c1c45433938d01d723856b7
Keenan Brock <kbrock@redhat.com>
2014-08-18 23:02:18 -0400

    Introduce home page

--- app/controllers/print_servers_controller.rb
+++ app/controllers/print_servers_controller.rb
@@ -3,6 +3,13 @@ class PrintServersController < ApplicationController
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+    if params[:id]
+      show
+      render 'show'
+    end
+  end
+
   def show
     @server = PrintServer.new(:id => params[:id])
     @printers = @server.printers
